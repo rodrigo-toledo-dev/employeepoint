ac03b4df2b7df97eb3bb0a31454cc961a0cfcca7
rodrigo@rtoledo.inf.br <rodrigo@rtoledo.inf.br>
2020-03-15 15:28:32 -0300

    creating store and partial of reducers

--- src/components/TableEmployees.js
+++ src/components/TableEmployees.js
@@ -1,18 +1,21 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 import {EmployeeSelect, EmployeeImage} from '~/components';
+import {connect} from 'react-redux';
 
-const TableEmployees = () => {
-  const [employee, setEmployee] = useState(false);
-  const [operation, setOperation] = useState(false);
-  const [operations, setOperations] = useState(false);
-  const resetData = employeeName => {
-    setEmployee(employeeName);
-    setOperation(false);
-    setOperations(false);
-  };
+const TableEmployees = ({employees, dispatch }) => {
+  // const [employee, setEmployee] = useState(false);
+  // const [operation, setOperation] = useState(false);
+  // const [operations, setOperations] = useState(false);
+  // const resetData = employeeName => {
+  //   setEmployee(employeeName);
+  //   setOperation(false);
+  //   setOperations(false);
+  // };
   return (
     <View>
+      {this.employees.map((employee) => },
+      \
       <EmployeeSelect onPress={() => resetData('Izabele')}>
         <EmployeeImage source={require('~/assets/isabele.jpeg')} />
       </EmployeeSelect>
@@ -23,4 +26,8 @@ const TableEmployees = () => {
   );
 };
 
-export default TableEmployees;
+const mapStateToProps => state({
+
+});
+
+export default connect()(TableEmployees);
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+
+const INTIAL_STATE = ['Fazer Cafe', 'Estudar'];
+
+function reducer(state= INTIAL_STATE, <i class="fas fa-acquisitions-incorporated    "></i>){
+  return state;
+}
+
+const store = createStore(reducer);
+
+export default store;
